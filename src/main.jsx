b40719afdffeb696337ed21c27053fd00b48a821
KevalDhamecha <kdhamecha541@rku.ac.in>
2024-12-31 08:16:28 +0530

    create state_manage function and how to use it

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import Component1 from './lect2412/component1'
 // import InpuutTest from './Component3'
-import ClassTimeTable from './lect2412/class_timetable'
+// import ClassTimeTable from './lect2412/class_timetable'
+import StateMerge from './lect2412/state_managment'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <Component1/> */}
     {/* <InpuutTest/> */}
-    <ClassTimeTable/>
+    {/* <ClassTimeTable/> */}
+    <StateMerge/>
   </StrictMode>,
 )
